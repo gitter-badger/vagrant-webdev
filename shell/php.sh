c209a5568af2e9461e7d78196440a7b8258ae81b
Dmytro Vasylchenko <dmytro@vasylchenko.com>
2014-07-27 13:52:59 +0300

    Enable php mcrypt module

--- shell/php.sh
+++ shell/php.sh
@@ -1,5 +1,7 @@
 apt-get install -y php5-fpm php5-cli php5-curl php5-xdebug php5-memcached php5-mongo php5-mcrypt
 
+php5enmod mcrypt
+
 sed -i 's/display_errors = .*/display_errors = On/' /etc/php5/fpm/php.ini
 sed -i 's/display_errors = .*/display_errors = On/' /etc/php5/cli/php.ini
 sed -i 's/error_reporting = .*/error_reporting = E_ALL/' /etc/php5/fpm/php.ini
