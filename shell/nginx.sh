dcd04432473c39e5f683b48c8a5a9f9bfd1451d0
Dmytro Vasylchenko <dmytro@vasylchenko.com>
2014-11-04 19:11:33 +0200

    Add 'sendfile off' to nginx.conf

--- shell/nginx.sh
+++ shell/nginx.sh
@@ -4,4 +4,6 @@ apt-get install -y nginx
 cp /vagrant/vagrant-webdev/data/nginx/dev.conf /etc/nginx/sites-available/dev.conf
 ln -s /etc/nginx/sites-available/dev.conf /etc/nginx/sites-enabled/dev.conf
 
+sed -i 's/sendfile on/sendfile off/' /etc/nginx/nginx.conf
+
 service nginx restart
